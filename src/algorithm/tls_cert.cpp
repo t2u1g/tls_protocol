e299811460b8d116100ad26f94444764dd464cca
_ <1509449084@qq.com>
2026-04-25 20:50:45 +0800

    add ASN.1 Language support

--- src/algorithm/tls_cert.cpp
+++ src/algorithm/tls_cert.cpp
@@ -17,25 +17,6 @@ static inline size_t read_rvs_num(const uint8_t *p, size_t length) {
     return ret;
 }
 
-enum ASN_FRAG_TYPE : uint8_t {
-    ASN_RESERVE = 0x00,
-    ASN_INTEGER = 0x02,
-    ASN_SEQUENCE = 0x10,
-};
-
-enum ASN_FRAG_TAG_TYPE : uint8_t {
-    ASN_Universal = 0x00,
-    ASN_Application = 0x40,
-    ASN_Context = 0x80,
-    ASN_Private = 0xC0
-};
-
-struct ASN_FRAG_TAG {
-    ASN_FRAG_TAG_TYPE tag_type;
-    uint8_t tag_number;
-    bool is_constru;
-};
-
 /**
  * This is a simplified ASN parsing implementation.
  * It does not support variable-length parsing with
@@ -47,13 +28,6 @@ struct ASN_FRAG_TAG {
  * long integer structure implementations, and then
  * modify ASN_length_parser and ASN_tag_parser
  */
-
-struct ASN_FRAG {
-    ASN_FRAG_TAG tag;
-    ASN_FRAG_TYPE type;
-    size_t length;
-    const uint8_t *p_frag_data;
-};
 
 size_t ASN_length_parser(const uint8_t *p_len, size_t max_length, size_t *result) {
     if (0 == max_length) {
@@ -118,16 +92,6 @@ size_t ASN_frag_parser(const uint8_t *asn_str, size_t max_length, ASN_FRAG *frag
 
 #define RET_CHECK(x) EXPR_CHECK((size_t)-1 == (x))
 
-struct CERT_UNPACK {
-    ASN_FRAG tbs_sign_alg;
-    ASN_FRAG issuer;
-    ASN_FRAG valid;
-    ASN_FRAG subject;
-    ASN_FRAG pub_key_info;
-    ASN_FRAG extens;
-    ASN_FRAG sign_val;
-};
-
 size_t x509v3_cert_unpack(void *cert, size_t cert_length, CERT_UNPACK *unpack) {
     const uint8_t *cert_ = (const uint8_t *)cert;
     ASN_FRAG *frag_arr[5] = {
@@ -156,31 +120,32 @@ size_t x509v3_cert_unpack(void *cert, size_t cert_length, CERT_UNPACK *unpack) {
         memcpy(frag_arr[i], &t_frag, sizeof(ASN_FRAG));
     }
 
-    for (size_t i = 0; i < 2; i++) {
+    do {
         RET_CHECK(ret = ASN_frag_parser(&cert_[offset], cert_length - offset, &t_frag));
-        if (ASN_Context == t_frag.type
+        offset += ret + t_frag.length;
+        if (ASN_Context == t_frag.tag.tag_type
             && 0x03 == t_frag.tag.tag_number) {
+            memcpy(&unpack->extens, &t_frag, sizeof(ASN_FRAG));
             break;
         }
+    } while (ASN_Universal != t_frag.tag.tag_type);
+
+    if (ASN_Universal != t_frag.tag.tag_type) {
+        RET_CHECK(ret = ASN_frag_parser(&cert_[offset], cert_length - offset, &t_frag));
         offset += ret + t_frag.length;
     }
-    memcpy(&unpack->extens, &t_frag, sizeof(ASN_FRAG)); // save extends.
-    offset += ret + t_frag.length;
-
-    RET_CHECK(ret = ASN_frag_parser(&cert_[offset], cert_length - offset, &t_frag));
     for (size_t i = 0; i < t_frag.length; i++) {
         if (t_frag.p_frag_data[i] != unpack->tbs_sign_alg.p_frag_data[i]) {
             return (size_t)-1;
         }
     }
-    offset += ret + t_frag.length;
     RET_CHECK(ret = ASN_frag_parser(&cert_[offset], cert_length - offset, &t_frag));
     memcpy(&unpack->sign_val, &t_frag, sizeof(ASN_FRAG)); // save signature value.
     return 0;
 }
 
 static inline void write_shift_bit_str(
-    uint8_t* dst, const uint8_t* src, 
+    uint8_t *dst, const uint8_t *src,
     size_t length, uint8_t val) {
     ;
     size_t shift_bytes = val / 8, shift_bits = val & 0x07;
@@ -193,14 +158,78 @@ static inline void write_shift_bit_str(
     }
 }
 
-size_t x509v3_cert_get_sign(CERT_UNPACK *pack, uint8_t *val_buff, size_t max_length) {
-    size_t total_length = pack->valid.length - 1;
+size_t ASN_read_bitstr(ASN_FRAG *frag, uint8_t *val_buff, size_t max_length) {
+    size_t total_length = frag->length - 1;
+    EXPR_CHECK(ASN_Universal == frag->tag.tag_type
+               && ASN_BITSTRING == frag->type);
     if (max_length < total_length) {
         return 0;
     }
-    write_shift_bit_str(val_buff, &pack->sign_val.p_frag_data[1],
-                        total_length, pack->sign_val.p_frag_data[0]);
-    return total_length - pack->sign_val.p_frag_data[0] / 8;
+    write_shift_bit_str(val_buff, &frag->p_frag_data[1],
+                        total_length, frag->p_frag_data[0]);
+    return total_length - frag->p_frag_data[0] / 8;
+}
+
+// this function not safe. uint32_t might overflow.
+static inline size_t read_base128_str(const uint8_t *str, size_t max_length, uint32_t *ret_val) {
+    uint32_t ret = 0, read_bytes = 0;
+    while (str[read_bytes] & 0x80 != 0
+           && read_bytes <= max_length) {
+        ret *= 128;
+        ret += str[0] & (~0x80);
+        read_bytes++;
+    }
+    if (read_bytes + 1 > max_length) {
+        return (uint32_t)-1;
+    }
+    ret *= 128;
+    ret += str[0] & (~0x80);
+    ret_val[0] = ret;
+    return read_bytes + 1;
+}
+
+size_t ASN_read_oid(ASN_FRAG *frag, ASN_OID *oid) {
+    EXPR_CHECK(ASN_Universal == frag->tag.tag_type
+               && ASN_OBJECT_IDENTIFIER == frag->type
+               && frag->length > 1);
+    size_t offset = 1;
+    oid->oid_val[0] = frag->p_frag_data[0] >> 6;
+    oid->oid_val[1] = frag->p_frag_data[0] & 0x3F;
+    oid->oid_num = 2;
+    while (offset < frag->length) {
+        EXPR_CHECK(oid->oid_num < 16);
+        size_t ret_ = read_base128_str(&frag->p_frag_data[offset],
+                                       frag->length - offset, 
+                                       &oid->oid_val[oid->oid_num]);
+        RET_CHECK(ret_);
+        offset += ret_;
+        oid->oid_num++;
+    }
+    return offset;
+}
+
+size_t x509v3_read_name(ASN_FRAG *frag) {
+    // unused;
+    return size_t();
+}
+
+size_t x509v3_read_date(ASN_FRAG *frag) {
+    // unused.
+    return size_t();
+}
+
+// this function signed data with cert.
+size_t x509v3_signed_with_cert(
+    CERT_UNPACK *cert, 
+    const uint8_t *data, size_t data_length, 
+    const uint8_t **res) {
+    ;
+
+}
+
+// this function check cert with another cert
+size_t x509v3_check_cert_sign(CERT_UNPACK *cert, CERT_UNPACK *ca) {
+    ;
 }
 
 #undef RET_CHECK
